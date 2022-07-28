6354969679c2a3a7d974f71496ea64325e4c2250
Brady Ratzmann <ratzmbra@huhs.org>
2022-07-28 10:55:44 -0500

    set up and added test1 item

---
+++ src/main/java/Blackout6868/RPGcraft/item/ModItems.java
@@ -0,0 +1,22 @@
+package blackout6868.rpgcraft.item;
+
+import blackout6868.rpgcraft.mcrpg;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, mcrpg.MOD_ID);
+
+    public static final RegistryObject<Item> TEST1 = ITEMS.register("test1",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
